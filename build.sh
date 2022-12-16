fa91044aec7d59ee4319fd23d2f0d2c9f07fa82b
mchonofsky <mchonofsky@gmail.com>
2022-12-16 01:08:36 -0500

    neuter build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 echo "running setup script"
-echo $DATABASE_URL
-psql $DATABASE_URL -f db/00_create_tables.sql
-psql $DATABASE_URL -f db/01_create_index_train_reports.sql
-psql $DATABASE_URL -f db/02_enter_stations.sql
-psql $DATABASE_URL -f db/04_add_all_stations.sql
-psql $DATABASE_URL -f db/05_add_more_stations_from_json.sql
-psql $DATABASE_URL -f db/06_add_additional_stations.sql
+echo "database url is" $DATABASE_URL
+# psql $DATABASE_URL -f db/00_create_tables.sql
+# psql $DATABASE_URL -f db/01_create_index_train_reports.sql
+# psql $DATABASE_URL -f db/02_enter_stations.sql
+# psql $DATABASE_URL -f db/04_add_all_stations.sql
+# psql $DATABASE_URL -f db/05_add_more_stations_from_json.sql
+# psql $DATABASE_URL -f db/06_add_additional_stations.sql
