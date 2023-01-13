3c06680b664419b29fdc2e41eb1e67af9780ea9f
mchonofsky <mchonofsky@gmail.com>
2023-01-13 01:08:04 -0500

    add rudimentary db versioning system

--- build.sh
+++ build.sh
@@ -1,8 +1,31 @@
 echo "running setup script"
+if [ -z ${HEROKU_POSTGRESQL_AQUA_URL+x} ]; then
+    echo 'USING LOCAL DB'
+    export DATABASE_URL='postgres://mchonofsky:mchonofsky@localhost:5432/amtrak-lol'
+else
+    echo 'HEROKU AQUA DEFINED'
+    export DATABASE_URL=$HEROKU_POSTGRESQL_AQUA_URL
+fi
 echo "database url is" $DATABASE_URL
-# psql $DATABASE_URL -f db/00_create_tables.sql
-# psql $DATABASE_URL -f db/01_create_index_train_reports.sql
-# psql $DATABASE_URL -f db/02_enter_stations.sql
-# psql $DATABASE_URL -f db/04_add_all_stations.sql
-# psql $DATABASE_URL -f db/05_add_more_stations_from_json.sql
-# psql $DATABASE_URL -f db/06_add_additional_stations.sql
+echo 'Executing Database build'
+
+############# DATABASE VERSIONING CONTROLLED BY TABLE versions
+#
+#
+#
+
+version_array=$( psql $DATABASE_URL -c 'copy (select migration_file from versions order by version_id) TO STDOUT;');
+
+for item in $(ls db/*.sql | sort) ; do
+    execute_file='true'
+    for migration in $version_array ; do
+        if [ $migration = item ]; then
+            execute_file='false'
+        fi
+    done
+    if [ $execute_file = 'true' ]; then
+        psql $DATABASE_URL -f $item;
+        psql -a amtrak-lol -c "insert into versions ( migration_file) values ('${item}') ;"
+    fi
+done
+
