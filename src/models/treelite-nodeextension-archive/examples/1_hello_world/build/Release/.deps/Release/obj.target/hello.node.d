01bc1eac7207f5ab1744b33d1095dc7aca0e09c9
mchonofsky <mchonofsky@gmail.com>
2023-03-14 10:00:36 +0000

    remove examples in treelite extension

--- src/models/treelite-nodeextension-archive/examples/1_hello_world/build/Release/.deps/Release/obj.target/hello.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/hello.node := g++ -o Release/obj.target/hello.node -shared -pthread -rdynamic -m64  -Wl,-soname=hello.node -Wl,--start-group Release/obj.target/hello/hello.o -Wl,--end-group 
